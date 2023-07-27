543c75e9937042909fed9ddc32565f4957e1802c
nozwock <57829219+nozwock@users.noreply.github.com>
2023-07-28 04:51:06 +0530

    Add new gui files

---
+++ spotdl_gui/newGui.py
@@ -0,0 +1,18 @@
+import sys
+
+from PyQt6 import QtWidgets, uic
+from views.MainWindow import Ui_MainWindow
+
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, obj=None, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+        self.setWindowTitle("SpotDL GUI")
+
+
+app = QtWidgets.QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+app.exec()
