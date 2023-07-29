e386e3b844ef6ad69e458ac450daf20290957d50
nozwock <57829219+nozwock@users.noreply.github.com>
2023-07-29 18:53:09 +0530

    Buttoon

--- spotdl_gui/newGui.py
+++ spotdl_gui/newGui.py
@@ -2,13 +2,13 @@ import sys
 
 import qdarktheme
 from assets import resource
-from PyQt6 import QtWidgets, uic
+from PySide6 import QtWidgets
 from views.MainWindow import Ui_MainWindow
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, obj=None, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.setupUi(self)
         self.setWindowTitle("SpotDL GUI")
 
