487b1c18a5b3a86adf583a97b9ed25320e8b74b9
nozwock <57829219+nozwock@users.noreply.github.com>
2023-07-31 19:36:32 +0530

    Refactor imports

--- spotdl_gui/models/tracks_model.py
+++ spotdl_gui/models/tracks_model.py
@@ -1,7 +1,7 @@
 from PySide6 import QtCore
 from PySide6.QtCore import Qt
 
-from spotdl_gui.spotdl_api import Song
+from ..spotdl_api import Song
 
 
 class TracksModel(QtCore.QAbstractTableModel):
