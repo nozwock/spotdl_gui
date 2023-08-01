586a4f50a0caefb2748fff5f6dfd579e8b3bde4f
nozwock <57829219+nozwock@users.noreply.github.com>
2023-08-01 09:56:30 +0530

    Single source of truth for pkg version

--- spotdl_gui/__init__.py
+++ spotdl_gui/__init__.py
@@ -1,1 +1,19 @@
-__version__ = "0.2.1"
+from importlib.metadata import version
+
+try:
+    __version__ = version(__package__)
+except Exception:
+    # Package isn't installed
+
+    def pkg_version() -> str | None:
+        from pathlib import Path
+
+        import tomllib
+
+        with open(Path(__package__).parent.joinpath("pyproject.toml"), "rb") as f:
+            return tomllib.load(f)["tool"]["poetry"]["version"]
+
+    _version = pkg_version()
+    assert _version is not None
+    __version__ = _version
+    del _version
