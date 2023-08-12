3e3e8d6fc79707755d2b77290067af55296f1ccb
nozwock <57829219+nozwock@users.noreply.github.com>
2023-08-13 04:55:31 +0530

    Fix workflow

--- release/build.py
+++ release/build.py
@@ -6,28 +6,36 @@ import PyInstaller.__main__
 from spotdl_gui import __version__
 
 root_path = Path(__file__).parent.parent
-icon = "icon.icns" if platform.system() == "Darwin" else "icon.ico"
+on_macos = platform.system() == "Darwin"
+icon = "icon.icns" if on_macos else "icon.ico"
 
-PyInstaller.__main__.run(
-    [
-        "--name",
-        f"spotdl_gui-{__version__}-{platform.machine()}-{platform.system()}",
-        "--noupx",
-        "--onedir",
-        "--windowed",
-        "--noconfirm",
-        "--collect-data",
-        "pykakasi",
-        "--collect-data",
-        "ytmusicapi",
-        "--copy-metadata",
-        "spotdl_gui",
-        "--copy-metadata",
-        "spotdl",
-        "--splash",
-        str(root_path / "release/resources/splash.png"),
-        "-i",
-        str(root_path / f"release/resources/{icon}"),
-        "spotdl_gui/__main__.py",
-    ]
-)
+
+args = [
+    "--name",
+    f"spotdl_gui-{__version__}-{platform.machine()}-{platform.system()}",
+    "--noupx",
+    "--onedir",
+    "--windowed",
+    "--noconfirm",
+    "--collect-data",
+    "pykakasi",
+    "--collect-data",
+    "ytmusicapi",
+    "--copy-metadata",
+    "spotdl_gui",
+    "--copy-metadata",
+    "spotdl",
+    "-i",
+    str(root_path / f"release/resources/{icon}"),
+    "spotdl_gui/__main__.py",
+]
+
+if not on_macos:
+    args.extend(
+        [
+            "--splash",
+            str(root_path / "release/resources/splash.png"),
+        ]
+    )
+
+PyInstaller.__main__.run(args)
